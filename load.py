0c90d52c61d09fe28aa87fc375b56ffb62e9b64f
aleksandrscode <almaslovskis@gmail.com>
2014-01-28 23:16:49 +0000

    Resource functions.

---
+++ load.py
@@ -0,0 +1,26 @@
+import pyglet
+
+
+# Load the resources from the following folders,
+# then re-index the file resource locations.
+def load_resources():
+    pyglet.resource.path = ["res", "res/images",
+                            "res/videos", "res/fonts",
+                            "res/music", "res/maps"]
+    pyglet.resource.reindex()
+
+# Set the anchor points to the center of the image.
+def center_anchor(image):
+    image.anchor_x = image.width//2
+    image.anchor_y = image.height//2
+
+# Using center_anchor() set the anchor in the center,
+# then return the resulting sprite.
+def image_aligner(img, x_pos, y_pos, batch, group):
+    image_load = pyglet.image.load(img) # First load the image
+    center_anchor(image_load) # Then center the anchor points
+    image = pyglet.sprite.Sprite(image_load, x=x_pos, # Convert to sprite
+                                           y=y_pos,
+                                           batch=batch,
+                                           group=group)
+    return image
